70dd8177117d0010b1658ace3cd1560e53ea709d
BenMMcLean <BenMMcLean@users.noreply.github.com>
2024-05-04 03:00:35 +0000

    Apply automatic changes

---
+++ javadoc/v0.2.0/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"cl.emilym.contrastaccessibility","l":"ContrastAccessibility"},{"p":"cl.emilym.contrastaccessibility","l":"ContrastAccessibilityContext"}];updateSearchResults();
